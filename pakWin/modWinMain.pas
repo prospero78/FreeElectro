0ccbc9243a6288fa70527781cf7d6c6cc34cfe0e
prospero78su <prospero.78.su@gmail.com>
2017-01-14 10:13:34 +0200

    07 Файл формы winMain (код).

---
+++ pakWin/modWinMain.pas
@@ -0,0 +1,26 @@
+UNIT modWinMain;
+
+{$mode objfpc}{$H+}
+
+INTERFACE
+
+USES
+        Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+TYPE
+        TForm1 = CLASS(TForm)
+        PRIVATE
+                { private declarations }
+        PUBLIC
+                { public declarations }
+        END;
+
+VAR
+        Form1: TForm1;
+
+IMPLEMENTATION
+
+{$R *.lfm}
+
+END.
+
