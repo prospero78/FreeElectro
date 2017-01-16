123193c09899ca9c2adca748cfce11b5999b04ff
prospero78 <prospero.78.su@gmail.com>
2017-01-16 14:40:34 +0200

    30 Добавлен модуль modWinSize (размер проводов)

---
+++ pakWin/modWinSize.pas
@@ -0,0 +1,26 @@
+UNIT modWinSize;
+
+{$mode objfpc}{$H+}
+
+INTERFACE
+
+USES
+      Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+TYPE
+      TwinSize = CLASS(TForm)
+      PRIVATE
+            { private declarations }
+      PUBLIC
+            { public declarations }
+      END;
+
+VAR
+      winSize: TwinSize;
+
+IMPLEMENTATION
+
+{$R *.lfm}
+
+END.
+
