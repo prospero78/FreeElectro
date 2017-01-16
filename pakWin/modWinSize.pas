fb88ead88b0a72bbd5bd377068b43c72bd33baf0
prospero78 <prospero.78.su@gmail.com>
2017-01-16 16:16:47 +0200

    45 Не понял как подключать ресурсы, в итоге тупо от них пока отказался.

--- pakWin/modWinSize.pas
+++ pakWin/modWinSize.pas
@@ -5,26 +5,26 @@ UNIT modWinSize;
 INTERFACE
 
 USES
-      Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-      LResources;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  LResources;
 
 TYPE
 
-			{ TwinSize }
+  { TwinSize }
 
-      TwinSize = CLASS(TForm)
-						btn1WireAl: TButton;
-						btn1WireCu: TButton;
-						txtSize: TMemo;
-						PROCEDURE btn1WireCuClick(Sender: TObject);
-      PRIVATE
-            { private declarations }
-      PUBLIC
-            { public declarations }
-      END;
+  TwinSize = CLASS(TForm)
+    btn1WireAl: TButton;
+    btn1WireCu: TButton;
+    txtSize:    TMemo;
+    PROCEDURE btn1WireCuClick(Sender: TObject);
+  PRIVATE
+    { private declarations }
+  PUBLIC
+    { public declarations }
+  END;
 
 VAR
-      winSize: TwinSize;
+  winSize: TwinSize;
 
 IMPLEMENTATION
 
@@ -33,9 +33,34 @@ IMPLEMENTATION
 { TwinSize }
 
 PROCEDURE TwinSize.btn1WireCuClick(Sender: TObject);
-BEGIN
-      txtSize.Lines:= LoadResource ('WIRE_CU_1');
-end;
+  VAR
+    str: string;
+  BEGIN
+
+    str := '+------------------------+--------------------------------------+-----------------------------------+' + #13#10;
+    str += '|Сеченине токопроводящей |   Провода  Напряжение в 220 В        |      Напряжение в 380 В           |'
+      + #13#10;
+    str += '|     жилы, мм²          |                                      |                                   |' + #13#10;
+    str += '|------------------------+--------------------------------------+-----------------------------------+ ' + #13#10;
+    str += '|                        | Сила тока, А  |   Мощность тока, кВт | Сила тока, А |  Мощность тока, кВт| '
+      + #13#10;
+    str += '|------------------------+--------------------------------------+-----------------------------------+ ' + #13#10;
+    str += '|       1,5              |         19    |          4,1         |       16     |        10,5        |  ' + #13#10;
+    str += '|       2,5              |         27    |          5,9         |       25     |        16,5        |  ' + #13#10;
+    str += '|       4                |         38    |          8,3         |       30     |        19,8        |  ' + #13#10;
+    str += '|       6                |         46    |         10,1         |       40     |        26,4        |  ' + #13#10;
+    str += '|      10                |         70    |         15,4         |       50     |        33,0        |  ' + #13#10;
+    str += '|      16                |         85    |         18,7         |       75     |        49,5        |  ' + #13#10;
+    str += '|      25                |        115    |         25,3         |       90     |        59,4        |  ' + #13#10;
+    str += '|      35                |        135    |         29,7         |      115     |        75,9        |  ' + #13#10;
+    str += '|      50                |        175    |         38,5         |      145     |        95,7        |  ' + #13#10;
+    str += '|      70                |        215    |         47,3         |      180     |       118,8        |  ' + #13#10;
+    str += '|      95                |        260    |         57,2         |      220     |       145,2        |  ' + #13#10;
+    str += '|     120                |        300    |         66,0         |      260     |       171,6        |  ' + #13#10;
+    str += '+------------------------+--------------------------------------+-----------------------------------+  ' + #13#10;
+
+    txtSize.Lines.Clear;
+    txtSize.Lines[0] := str;
+  END;
 
 END.
-
